aae3e8493fea9f551dfae85bb4a9d54a9aec1170
saranatour1 <saranatourcontact@gmail.com>
2024-07-08 14:59:53 +0300

    chore(GEM-2): updated login page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import {nextui} from "@nextui-org/react";
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
@@ -18,6 +18,6 @@ const config: Config = {
     },
   },
   
-  plugins: [],
+  plugins: [nextui()],
 };
 export default config;
