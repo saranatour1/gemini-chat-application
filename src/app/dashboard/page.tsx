5cd2d5086c0f3b4e5cc2416971ac52692c6db65c
saranatour1 <saranatourcontact@gmail.com>
2024-07-13 12:47:10 +0300

    chore(GEM-3): remove package

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,8 @@
 'use client'
-
-import { Test } from "@/components/Test"
-import useAuth from "@/hooks/useAuth"
-import useNavigate from "@/hooks/useNavigate"
-import { useEffect } from "react"
-
 export default function page(){
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const nav = useNavigate()
 
   return (
     <div className="w-full max-w-[1024px] grid justify-center items-start grid-flow-col pt-6 gap-x-4">
-      <Test/>
       <div className="col-span-1 w-full max-w-[60px]">
       older messages
       </div>
--- src/utils/clientInstance.ts
+++
@@ -1,7 +0,0 @@
-// this should be a single instance
-import { StreamChat } from "stream-chat";
-
-export const serverClient = StreamChat.getInstance(
-  process.env.GOSTREAM_KEY as string,
-  process.env.GORSTRAM_SECRET_KEY as string,
-);
