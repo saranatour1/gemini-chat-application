3c6a4f430bc8f798ed2598186c6fffc0fdebffd0
saranatour1 <saranatourcontact@gmail.com>
2024-07-20 21:37:29 +0300

    chore(GEM-4): delete chat

---
+++ src/components/content/Alert.tsx
@@ -0,0 +1,44 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { ReactNode } from "react";
+
+export const Alert = ({
+  children,
+  open,
+  setOpen,
+  execute
+
+}: {
+  children: ReactNode;
+  open: boolean;
+  setOpen: (i: boolean) => void;
+  execute:()=>void
+}) => {
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your account and remove your data from our
+            servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={execute}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
--- src/components/content/MessagesArea.tsx
+++ src/components/content/MessagesArea.tsx
@@ -2,24 +2,15 @@ import { Id } from "../../../convex/_generated/dataModel";
 import { useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Delete, Edit } from "lucide-react";
+import { Button } from "../ui/button";
+import { ChannelInfo } from "./ChannelInfo";
 export const MessagesArea = ({ chatId }: { chatId: Id<"channels"> }) => {
   const messageViewer = useQuery(api.messages.viewer, chatId ? { chatId: chatId } : "skip");
-  const channelInfo = useQuery(api.chats.getChatById, chatId ? { chatId: chatId } : "skip");
   const user = useQuery(api.users.viewer);
   return (
     <div className="w-full h-full flex flex-col justify-end items-end">
-      {/* Todo: add edit information */}
-      <div className="w-full self-start p-4 ">
-        <div className="flex items-center justify-start gap-x-4">
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-        <p>{channelInfo?.name}</p>
-        </div>
-
-      </div>
-
+      <ChannelInfo channelId={chatId}/>
       <div className="w-full h-[60vh] overflow-scroll self-end mt-auto p-4 grid grid-flow-row auto-rows-max gap-y-2">
         {messageViewer?.map((item, idx) => (
           <p
