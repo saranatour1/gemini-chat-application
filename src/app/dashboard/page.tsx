e01431a2f29d5e2a5d03500f6747bf4f45ae3e49
saranatour1 <saranatourcontact@gmail.com>
2024-07-28 19:38:01 +0300

    chore(GEM-5): navigate to last added Id

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,5 @@
-"use client";
-import { Aside } from "@/components/SideBar/Aside";
-import { Button } from "@/components/ui/button";
-import { Authenticated, Unauthenticated } from "convex/react";
-import Link from "next/link";
-import { buttonVariants } from "@/components/ui/button";
-
 export default function page() {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-        {/* dahsboatd */}
-    </div>
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">{/* dahsboatd */}</div>
   );
 }
--- src/components/SignedIn.tsx
+++ src/components/SignedIn.tsx
@@ -6,14 +6,14 @@ import { Header } from "./Content/Header";
 
 export const SignedIn = ({ children }: Children) => {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
     <Authenticated>
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <Aside />
       <div className="flex flex-col">
       <Header />
       {children}
       </div>
-    </Authenticated>
     </div>
+    </Authenticated>
   );
 };
