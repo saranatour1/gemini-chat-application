57869162e7ca09fccea2a3c3aa64066d319b8a7c
saranatour1 <saranatourcontact@gmail.com>
2024-07-28 11:00:45 +0300

    chore(GEM-5): update settings schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,6 +5,69 @@ import { literals } from "convex-helpers/validators";
 
 const schema = defineSchema({
   ...authTables,
+  settings: defineTable({
+    userId: v.id("users"),
+    responseType: v.union(v.literal("chat"), v.literal("single-message")),
+    theme: v.union(v.literal("dark"), v.literal("light")),
+    keepChat: v.string(), // How long to keep the chats for // max of 30 days
+    attachments: v.union(
+      v.object({
+        audio: v.boolean(),
+      }),
+      v.object({
+        images: v.boolean(),
+      })
+    ),
+    model: v.union(
+      v.literal("gemini-1.5-pro"),
+      v.literal("gemini-1.5-flash"),
+      v.literal("gemini-1.0-pro"),
+      v.literal("text-embedding-004"),
+      v.literal("aqa")
+    ),
+    languages: v.union(
+      v.literal("ar"),
+      v.literal("bn"),
+      v.literal("bg"),
+      v.literal("zh"),
+      v.literal("hr"),
+      v.literal("cs"),
+      v.literal("da"),
+      v.literal("nl"),
+      v.literal("en"),
+      v.literal("et"),
+      v.literal("fi"),
+      v.literal("fr"),
+      v.literal("de"),
+      v.literal("el"),
+      v.literal("iw"),
+      v.literal("hi"),
+      v.literal("hu"),
+      v.literal("id"),
+      v.literal("it"),
+      v.literal("ja"),
+      v.literal("ko"),
+      v.literal("lv"),
+      v.literal("lt"),
+      v.literal("no"),
+      v.literal("pl"),
+      v.literal("pt"),
+      v.literal("ro"),
+      v.literal("ru"),
+      v.literal("sr"),
+      v.literal("sk"),
+      v.literal("sl"),
+      v.literal("es"),
+      v.literal("sw"),
+      v.literal("sv"),
+      v.literal("th"),
+      v.literal("tr"),
+      v.literal("uk"),
+      v.literal("vi")
+    ),
+  })
+    .index("userId", ["userId"])
+    .index("languages", ["languages"]),
   threads: defineTable({
     summary: v.optional(v.string()),
     summarizer: v.optional(v.id("_scheduled_functions")),
@@ -36,24 +99,18 @@ const schema = defineSchema({
   })
     .index("state", ["state", "author.userId"])
     .index("threadId", ["threadId"]),
-    jobs: defineTable({
-      work: v.object({
-        responseId: v.id("messages"),
-        stream: v.boolean(),
-      }),
-      status: literals(
-        "pending",
-        "inProgress",
-        "success",
-        "failed",
-        "timedOut",
-      ),
-      lastUpdate: v.number(),
-      workerId: v.optional(v.id("workers")),
-      janitorId: v.optional(v.id("_scheduled_functions")),
-      start: v.optional(v.number()),
-      end: v.optional(v.number()),
-    })
+  jobs: defineTable({
+    work: v.object({
+      responseId: v.id("messages"),
+      stream: v.boolean(),
+    }),
+    status: literals("pending", "inProgress", "success", "failed", "timedOut"),
+    lastUpdate: v.number(),
+    workerId: v.optional(v.id("workers")),
+    janitorId: v.optional(v.id("_scheduled_functions")),
+    start: v.optional(v.number()),
+    end: v.optional(v.number()),
+  })
     .index("responseId", ["work.responseId"])
     .index("status", ["status", "lastUpdate"]),
 });
