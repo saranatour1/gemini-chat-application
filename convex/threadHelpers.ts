02648a68fc43d033cdf6ee6e2140f0316a7c8bbd
Sara <77834808+saranatour1@users.noreply.github.com>
2024-08-02 14:50:03 +0300

    chore(GEM-16): limit time between created threads for 5 min (#24)

--- convex/threadHelpers.ts
+++ convex/threadHelpers.ts
@@ -1,6 +1,8 @@
+import { getManyFrom } from "convex-helpers/server/relationships";
 import { internal } from "./_generated/api";
 import { Doc, Id } from "./_generated/dataModel";
 import { MutationCtx, QueryCtx } from "./_generated/server";
+import { getMessagesFromThreadId } from "./messagesHelpers";
 
 // summarize thread through first messages
 export const summarizeThread = async (ctx: MutationCtx, threadId: Id<"threads">, settings:Doc<"settings">) => {
@@ -20,4 +22,20 @@ export const summarizeThread = async (ctx: MutationCtx, threadId: Id<"threads">,
 export const getEntireChat = async(ctx:QueryCtx,id:Id<"threads">) =>{
   const allMessages = await ctx.db.query("messages").withIndex("threadId",q=> q.eq("threadId",id)).collect();
   return allMessages
+}
+
+// check if the previous created thread has messages, and 5 min has passed 
+export const checkLastThreads = async (ctx:MutationCtx,userId:Id<"users">)=>{
+  let hasNewThreads = false; 
+  const fiveMinutesAgo = Date.now() - (5 * 60 * 1000);
+  const lastTwoThreads = await ctx.db.query("threadsMembers").withIndex("userId",q=>q.eq("userId",userId)).order("asc").take(2)
+  
+  for (const thread of lastTwoThreads) {
+    if (thread._creationTime >= fiveMinutesAgo) {
+      hasNewThreads = true;
+      break;
+    }
+  }
+  
+  return hasNewThreads
 }
