a71d741aa41289c3f608f5f0dccd38777347c1e6
Sara <77834808+saranatour1@users.noreply.github.com>
2024-08-11 20:08:27 +0300

    chore(GEM-23): update auth package (#33)

--- src/components/Provider/Provider.tsx
+++ src/components/Provider/Provider.tsx
@@ -1,10 +1,10 @@
 'use client'
 import { Children } from "@/types";
-import { ConvexAuthProvider } from "@convex-dev/auth/react";
+import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs";
 import { ConvexReactClient } from "convex/react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export const Provider = ({ children }: Children) => {
-  return <ConvexAuthProvider client={convex}>{children}</ConvexAuthProvider>;
+  return <ConvexAuthNextjsProvider client={convex}>{children}</ConvexAuthNextjsProvider>;
 };
