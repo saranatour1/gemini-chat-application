3fc788a038a827130110a5082e6f2e5d11be6a81
saranatour1 <saranatourcontact@gmail.com>
2024-12-06 15:33:28 +0200

    update auth package to use server components

--- src/components/Provider/Provider.tsx
+++ src/components/Provider/Provider.tsx
@@ -3,8 +3,9 @@ import { Children } from "@/types";
 import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { ConvexReactClient } from "convex/react";
 
+import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export const Provider = ({ children }: Children) => {
-  return <ConvexAuthProvider client={convex}>{children}</ConvexAuthProvider>;
+  return <ConvexAuthNextjsProvider client={convex}>{children}</ConvexAuthNextjsProvider>;
 };
