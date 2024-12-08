b9f089502cb38f99f0e4e7e3aa9208795950f19a
saranatour1 <saranatourcontact@gmail.com>
2024-12-08 17:24:45 +0200

    fix type issue

--- src/app/dashboard/settings/account/constants.ts
+++ src/app/dashboard/settings/account/constants.ts
@@ -56,14 +56,32 @@ export const accountFormSchema = (settings: Doc<"settings">) => {
       images: z.boolean().default(settings?.attachments.images ?? false),
     }),
     model: z
-      .enum(["gemini-1.0-pro","gemini-1.0-pro-latest","gemini-1.0-pro-001","gemini-1.5-flash","gemini-1.5-flash-latest","gemini-1.5-pro-latest","gemini-1.5-pro"])
-      .default(settings?.model ?? "gemini-1.0-pro-latest"),
+      .enum(["gemini-1.5-flash-001",
+        "gemini-1.5-flash-002",
+        "gemini-1.5-pro-001",
+        "gemini-1.5-pro-002",
+        "gemini-1.0-pro-001",
+        "gemini-1.0-pro-vision-001",
+        "gemini-1.0-pro",
+        "gemini-1.0-pro-001",
+        "gemini-1.0-pro-002"])
+      .default(settings?.model ?? "gemini-1.5-pro-001"),
     languages: z.enum(languagesSet).default(settings?.languages ?? "en"),
   });
 };
 
-export const responses: readonly string[] = ["chat", "single-message"];
-export type Response = "chat"| "single-message"
+export const responses = ["chat", "single-message"] as const 
+export type Response = typeof responses[number]
 
-export const models:readonly string[] = ["gemini-1.0-pro","gemini-1.0-pro-latest","gemini-1.0-pro-001","gemini-1.5-flash","gemini-1.5-flash-latest","gemini-1.5-pro-latest","gemini-1.5-pro"]
-export type Model = "gemini-1.0-pro"|"gemini-1.0-pro-latest"|"gemini-1.0-pro-001"|"gemini-1.5-flash"|"gemini-1.5-flash-latest"|"gemini-1.5-pro-latest"|"gemini-1.5-pro"
+export const models = ["gemini-1.5-flash-001",
+  "gemini-1.5-flash-002",
+  "gemini-1.5-pro-001",
+  "gemini-1.5-pro-002",
+  "gemini-1.0-pro-001",
+  "gemini-1.0-pro-vision-001",
+  "gemini-1.0-pro",
+  "gemini-1.0-pro-001",
+  "gemini-1.0-pro-002"] as const
+
+  export type Model = typeof models[number];
+
